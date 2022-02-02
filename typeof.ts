cb2383cb6cfbec30cf9fa85ab163a39cf60b48f1
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-02 17:59:26 -0500

    🔥 Chore: Adds type safety for the parameters or vars

---
+++ typeof.ts
@@ -0,0 +1,4 @@
+// In TypeScript, you can use the typeof operator in a type context to refer to the type of a property or a variable.
+
+let greeting = "hello";
+let typeOfGreeting: typeof greeting; // similar to let typeOfGreeting: string
