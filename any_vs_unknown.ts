24c8d2bb42d8579057a47052820fa2c7926fca2d
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-02 18:13:34 -0500

    🔥 Chore: Any can be unknown, unknown can be any. unkown cant be string unless narrowed down

---
+++ any_vs_unknown.ts
@@ -0,0 +1,14 @@
+let foo: unknown = "Akshay";
+
+let foo2: any = "Bar";
+
+let bar: string = foo; // Type 'unknown' is not assignable to type 'string'.(2322)
+
+let bar2: string = foo as string; // Narrowed down to string to work
+
+foo2 = foo; // Any can hold unknown
+foo = foo2; // Unknown can also hold any
+
+let newString: string = foo2; // Any can be assign to any types
+let newUnkown: string = foo; // Unknown can't be assigned to any types
+let narrowedNewUnkown: string = foo as string; // Unless we narrow down the unknown to the same type
