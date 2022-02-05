b10a05b283bc5a6ff392c732578148e69fe4cf80
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-04 23:26:47 -0500

    🔥 Chore: <T> Generics

---
+++ chaining.ts
@@ -0,0 +1,27 @@
+// Generics
+// 'T' is going to be a type declared at run-time instead of compile time.
+// The T variable could be any non-declared variable (I couldn't find a reference, but I would assume any valid set of characters that could be used for a variable names).
+export class Lodash<T extends object> {
+  constructor(private obj: T) {}
+
+  subset<K extends keyof T>(...keys: K[]): Lodash<Pick<T, K>> {
+    return new Lodash(
+      keys.reduce(
+        (acc, key) => ({ ...acc, [key]: this.obj[key] }),
+        {} as Pick<T, K>
+      )
+    );
+  }
+
+  get<K extends keyof T>(key: K): T[K] {
+    return this.obj[key];
+  }
+}
+
+const obj = new Lodash({
+  foo: "bar",
+  bar: "baz",
+  hello: "world",
+});
+
+obj.subset("foo", "bar").get("hello");
