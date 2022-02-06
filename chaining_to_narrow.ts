05cf2c726ecc3a0b4ccb571d4fc6c17394f9d1c4
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-05 21:22:00 -0500

    📄 Note: type chaining to narrow types

---
+++ chaining_to_narrow.ts
@@ -0,0 +1,28 @@
+// USE Method chaining to narrow types
+// RETURN a new instance from every method using a more specific generic
+// GET Immutable interface
+
+class Lodash<T extends object> {
+  constructor(private obj: T) {}
+
+  subset<K extends keyof T>(...keys: K[]): Lodash<Pick<T, K>> {
+    return new Lodash(
+      keys.reduce(
+        (acc, key) => ({ ...acc, [key]: this.obj[key] }),
+        {} as Pick<T, K>
+      )
+    );
+  }
+
+  get<K extends keyof T>(key: K): T[K] {
+    return this.obj[key];
+  }
+}
+
+const obj = new Lodash({
+  foo: "bar",
+  bar: "baz",
+  hello: "world",
+});
+
+obj.subset("foo", "bar").get("hello"); // 💥
