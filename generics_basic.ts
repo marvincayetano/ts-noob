6910e6b577c70bcf7b37db4851bd2766313ef92c
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-07 21:38:36 -0500

    📄 Note: Very basic generic Type variable

---
+++ generics_basic.ts
@@ -0,0 +1,7 @@
+// While using any is certainly generic in that it will cause the function to accept any and all types for the type of arg, we actually are losing the information about what that type was when the function returns. If we passed in a number, the only information we have is that any type could be returned.
+
+// Instead, we need a way of capturing the type of the argument in such a way that we can also use it to denote what is being returned. Here, we will use a type variable, a special kind of variable that works on types rather than values.
+
+function identity<Type>(arg: Type): Type {
+  return arg;
+}
