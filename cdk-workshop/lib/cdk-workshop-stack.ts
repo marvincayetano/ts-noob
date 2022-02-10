ad51e8220871e8827f4225a488abde3fa010f40a
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-10 18:51:11 -0500

    🔥 Chore: Awesome API Gateway

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import * as lambda from "aws-cdk-lib/aws-lambda";
+import * as apigw from "aws-cdk-lib/aws-apigateway";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
@@ -11,6 +12,11 @@ export class CdkWorkshopStack extends Stack {
       runtime: lambda.Runtime.NODEJS_14_X, // Execution Environment - Basically what runs the code
       code: lambda.Code.fromAsset("lambda"), // Code inside the lambda folder
       handler: "hello.handler", // Name of the file function inside the lambda folder file
+    });
+
+    // API Gateway REST API for our hello
+    new apigw.LambdaRestApi(this, "Endpoint", {
+      handler: hello,
     });
   }
 }
