1387cb5bc748c3a0099b28643410d7c785205702
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-10 18:10:08 -0500

    🔥 Chore: Simple hello lambda

--- cdk-workshop/lib/cdk-workshop-stack.ts
+++ cdk-workshop/lib/cdk-workshop-stack.ts
@@ -1,8 +1,16 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
+import * as lambda from "aws-cdk-lib/aws-lambda";
 
 export class CdkWorkshopStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    // defines an AWS Lambda Resource
+    const hello = new lambda.Function(this, "HelloHandler", {
+      runtime: lambda.Runtime.NODEJS_14_X, // Execution Environment - Basically what runs the code
+      code: lambda.Code.fromAsset("lambda"), // Code inside the lambda folder
+      handler: "hello.handler", // Function inside the lambda folder files
+    });
   }
 }
