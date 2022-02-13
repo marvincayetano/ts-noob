8b308462175afa8f7a98257cf99a0da86e49ef28
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-02-12 19:03:27 -0500

    🔥 Chore: Added readcapacity checker in constructor

--- cdk-workshop/lib/hitcounter.ts
+++ cdk-workshop/lib/hitcounter.ts
@@ -25,11 +25,19 @@ export class HitCounter extends Construct {
   public readonly table: dynamodb.Table;
 
   constructor(scope: Construct, id: string, props: HitCounterProps) {
+    if (
+      props.readCapacity !== undefined &&
+      (props.readCapacity < 5 || props.readCapacity > 20)
+    ) {
+      throw new Error("readCapacity must be greater than 5 and less than 20");
+    }
+
     super(scope, id);
 
     const table = new dynamodb.Table(this, "Hits", {
       partitionKey: { name: "path", type: dynamodb.AttributeType.STRING },
       encryption: dynamodb.TableEncryption.AWS_MANAGED,
+      readCapacity: props.readCapacity ?? 5,
     });
     this.table = table;
 
