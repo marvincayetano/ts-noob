87f880c7bb034b0a090fde44c51eaf75b3cb6757
Marvin Cayetano <cayetanomarvin@gmail.com>
2022-03-12 19:45:57 -0500

    🔥 Chore: Basic interface type

--- interface.ts
+++ interface.ts
@@ -8,3 +8,31 @@ interface Employee {
 function process(employee: Employee) {
   console.log(`${employee.name}'s salary = ${employee.salary}`);
 }
+
+export type User = unknown;
+
+export type User = {
+  name: string;
+  age: number;
+  occupation: string;
+};
+
+export const users: User[] = [
+  {
+    name: "Max Mustermann",
+    age: 25,
+    occupation: "Chimney sweep",
+  },
+  {
+    name: "Kate Müller",
+    age: 23,
+    occupation: "Astronaut",
+  },
+];
+
+export function logPerson(user: User) {
+  console.log(` - ${user.name}, ${user.age}`);
+}
+
+console.log("Users:");
+users.forEach(logPerson);
